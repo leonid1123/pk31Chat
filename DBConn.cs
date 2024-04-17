778ca2511c202be07af5ef15fc0b7f637c6c25a9
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2024-04-17 14:54:02 +0300

    Добавьте файлы проекта.

---
+++ DBConn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace pk31Chat
+{
+    public static class DBConn
+    {
+        static MySqlConnection connection = new MySqlConnection();
+        public static MySqlConnection Connect()
+        {
+            //192.168.1.61
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection = new MySqlConnection("Server=localhost;User ID=pk31;Password=pk31;Database=chat");
+                connection.Open();
+                Console.WriteLine(connection.State);
+                return connection;
+            } else
+            {
+                return null;
+            }
+        }
+        public static void Close()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pk31Chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string loginInput = textBox1.Text.Trim();
+            if (loginInput.Length>2 && loginInput.Length<21)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = DBConn.Connect();
+                cmd.CommandText = "SELECT `id`, `password` FROM `users` WHERE `login`=@userInput";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("userInput",loginInput);
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("есть такое");
+                } else
+                {
+                    MessageBox.Show("нет такое");
+                }
+                DBConn.Close();
+            }
+            else
+            {
+                MessageBox.Show("Логин должен быть от 3 до 20 символов");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+        }
+    }
+}
